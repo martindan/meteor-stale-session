a3720eb7eda17a4505ca9c80a39fe407042c88d2
martin_dang <martin.dang@sirma.bg>
2018-04-24 17:56:34 +0300

    update versions

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name:    'zuuk:stale-session',
+  name:    'martindan:stale-session',
   summary: 'Stale session and session timeout handling for meteorjs',
-  git:     "https://github.com/lindleycb/meteor-stale-session.git",
+  git:     "https://github.com/martindan/meteor-stale-session.git",
   version: "1.0.8"
 });
 
 Package.onUse(function(api) {
     api.use('accounts-base@1.0.0', ['client','server']);
-    api.use('jquery@1.0.0', 'client');
+    api.use('jquery@3.3.1', 'client');
     api.addFiles('client.js', 'client');
     api.addFiles('server.js', 'server');
 });
